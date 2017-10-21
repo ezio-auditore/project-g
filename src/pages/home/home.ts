96a16d250703ca5ca19155d7902b62246a9d85ae
Kaustav Majumder <pupunmajumder@gmail.com>
2017-10-21 14:40:39 +0530

    Setting up Http Client

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,53 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, NavParams , LoadingController} from 'ionic-angular';
+import {DataProvider} from '../../providers/data/data';
+import {Storage} from '@ionic/storage';
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+games = [];
+genre :any;
+favourites =[];
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams ,private _dataProvider :DataProvider,
+     public storage :Storage,public _loading :LoadingController) {
+       let loader = this._loading.create({
+         content : 'Getting games'
+       })
 
-  constructor(public navCtrl: NavController) {
+       loader.present().then(() => {
+         this.storage.get('genre').then(val =>{
+           if(val)
+            {
 
+              this.genre = {"id":val.id,"name":val.name};
+            }
+            else{
+
+              this.genre = {"id":5,"name":"Shooter Games"};
+              this.storage.set('genre',this.genre);
+            }
+
+            this._dataProvider.getGames(this.genre.id,0)
+              .subscribe(res => this.games = res);
+         });
+         this.storage.get('favourites').then(val => {
+           if(!val){
+             this.storage.set('favourites',this.favourites);
+           }
+           else{
+             this.favourites = val;
+           }
+         })
+         loader.dismiss();
+       });
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
   }
 
 }
--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -8,5 +8,22 @@ export class DataProvider {
   constructor(public http: Http) {
     console.log('Hello DataProvider Provider');
   }
+  limit: number = 30;
+  games_api_url = '/games/';
+  cors_url="https://cors.io"
+  api_key = 'e61f7617a717227258282a6a985c3f05';
+  headers = new Headers({ 'user-key': 'e61f7617a717227258282a6a985c3f05' ,'Access-Control-Allow-Origin' :'*'} );
+  options = new RequestOptions({ headers: this.headers });
 
+  getGames(genre, offset_num) {
+    let genre_id = genre;
+    let offset = offset_num;
+
+    return this.http.get(`${this.games_api_url}?fields=name,release_dates&limit=${this.limit}&offset=${offset}&order=release_dates.date:desc&filter[genres][eq]=${genre_id}`,this.options)
+      .map(res => res.json());
+  }
+  getGamesProxy(genre,offset){
+    return this.http.get('/assets/game-data.json')
+    .map(res => res.json());
+  }
 }
